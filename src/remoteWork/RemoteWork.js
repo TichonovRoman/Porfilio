77e0d45606e9c06024fa4faf1d380fe629853c5e
TichonovRoman <romikt@inbox.ru>
2022-03-28 11:22:50 +0300

    add RemoteWork

---
+++ src/remoteWork/RemoteWork.js
@@ -0,0 +1,20 @@
+import React from "react";
+import s from "./RemoteWork.module.css"
+
+export const RemoteWork = () => {
+    return (
+        <div className={s.container1}>
+            <div className={s.worksBlock}>
+                <div className={s.container2}>
+                    <h2 className={s.title}>I am considering options for remote work</h2>
+                    <button className={s.button}>Recruit me</button>
+                </div>
+
+
+            </div>
+
+        </div>
+    )
+}
+
+
